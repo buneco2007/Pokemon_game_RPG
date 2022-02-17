3884b158de6691cd7e0f546ea2714fd53f375495
buneco2007 <buneco2007@gmail.com>
2022-02-16 23:05:58 -0300

    Inclusao da classe agua

--- classes.py
+++ classes.py
@@ -6,7 +6,10 @@ import tipos
 
 
 class Aco(tipos.Tipos):
-    """Classe que pokemon do tipo aco
+    """Classe de pokemon aço
+
+    Args:
+        tipos (Str): Tipo do pokemon
     """
     tipo: str = tipos.Tipos.aco
     normal: Any = [tipos.Tipos.bicho, tipos.Tipos.dragao, tipos.Tipos.fantasma,
@@ -18,3 +21,20 @@ class Aco(tipos.Tipos):
     forte: List = [tipos.Tipos.fada, tipos.Tipos.gelo,
                    tipos.Tipos.rocha]
     imune: List = []
+
+
+class Agua(tipos.Tipos):
+    """Classe de pokemon de água
+
+    Args:
+        tipos (str): Tipo de pokemon
+    """
+    tipo: str = tipos.Tipos.agua
+    normal: Any = [tipos.Tipos.aco, tipos.Tipos.bicho, tipos.Tipos.eletrico,
+                   tipos.Tipos.fantasma, tipos.Tipos.fada, tipos.Tipos.gelo,
+                   tipos.Tipos.luta, tipos.Tipos.normal, tipos.Tipos.psiquico,
+                   tipos.Tipos.sinistro, tipos.Tipos.veneno, tipos.Tipos.voador]
+    fraco: list[str] = [tipos.Tipos.agua,
+                        tipos.Tipos.dragao, tipos.Tipos.planta]
+    forte: list[str] = [tipos.Tipos.fogo, tipos.Tipos.rocha, tipos.Tipos.rocha]
+    imune: list[str] = []
